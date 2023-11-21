d08410fd1a39cef5558d45401e1a959f1b526794
save <save>
2023-11-21 19:04:01 +0000

    new file:   .github/workflows/unittest.yml 	new file:   app/Makefile 	new file:   app/__init__.py 	new file:   app/app.py 	new file:   app/requirements.txt 	new file:   app/site.db 	new file:   app/templates/index.html 	new file:   app/test/__init__.py 	new file:   app/test/test.py

---
+++ app/app.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'  
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+app.config['ENV'] = 'development'
+app.config['DEBUG'] = True
+
+db = SQLAlchemy(app)
+
+class Playlist(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    artist_name = db.Column(db.String(100), nullable=False)
+    track_id = db.Column(db.String(50), nullable=False)
+    date_added = db.Column(db.DateTime, default=datetime.utcnow)
+
+@app.route('/')
+def index():
+    playlists = Playlist.query.all()
+    return render_template('index.html', playlists=playlists)
+
+
+if __name__ == '__main__':
+    db.drop_all()
+    db.create_all()
+    app.run()
+
---
+++ app/test/test.py
@@ -0,0 +1,42 @@
+import unittest
+from sys import path
+path.append('../')
+from app import app, db, Playlist
+
+
+class TestFlaskApp(unittest.TestCase):
+
+    def setUp(self):
+        app.config['TESTING'] = True
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///:memory:'
+        self.app = app.test_client()
+        db.create_all()
+
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
+
+    def test_index_route(self):
+        response = self.app.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b'Playlist Table', response.data)
+
+    def test_empty_playlist(self):
+        response = self.app.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b'No playlists found', response.data)
+
+    def test_populated_playlist(self):
+        db.session.add(Playlist(artist_name='Artist 1', track_id='12345'))
+        db.session.commit()
+
+        response = self.app.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b'Playlist ID</th>', response.data)
+        self.assertIn(b'Artist 1', response.data)
+        self.assertIn(b'12345', response.data)
+
+
+if __name__ == '__main__':
+    unittest.main()
+
